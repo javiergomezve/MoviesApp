e279b0f222ac61040006e9a911cd4100f62ab749
Javier Gomez <javiergomezve@gmail.com>
2024-02-20 10:44:13 -0500

    Add HorizontalSlider component

---
+++ src/components/HorizontalSlider.tsx
@@ -0,0 +1,34 @@
+import {Movie} from '../interfaces/movieInterface.tsx';
+import {FlatList, Text, View} from 'react-native';
+import MoviePoster from './MoviePoster.tsx';
+import React from 'react';
+
+interface Props {
+  title?: string;
+  movies: Movie[];
+}
+
+const HorizontalSlider = ({title, movies}: Props) => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'red',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+      }}>
+      {title && <Text style={{fontSize: 30, fontWeight: 'bold'}}>{title}</Text>}
+
+      <FlatList
+        data={movies}
+        renderItem={({item}) => (
+          <MoviePoster movie={item} height={200} width={140} />
+        )}
+        keyExtractor={item => item.id.toString()}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default HorizontalSlider;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,58 +1,58 @@
-import React from "react";
-import { ActivityIndicator, Dimensions, FlatList, ScrollView, Text, View } from "react-native";
-import { StackScreenProps } from "@react-navigation/stack";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Carousel from "react-native-snap-carousel";
+import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
 
+import useMovies from '../hooks/useMovies.tsx';
+import MoviePoster from '../components/MoviePoster.tsx';
+import HorizontalSlider from '../components/HorizontalSlider.tsx';
 
-import useMovies from "../hooks/useMovies.tsx";
-import MoviePoster from "../components/MoviePoster.tsx";
+const {width: windowsWidth} = Dimensions.get('window');
 
-const { width: windowsWidth } = Dimensions.get("window");
-
-interface Props extends StackScreenProps<any, any> {
-}
+interface Props extends StackScreenProps<any, any> {}
 
 const HomeScreen = (props: Props) => {
-  const { navigation } = props;
+  const {navigation} = props;
 
-  const { top } = useSafeAreaInsets();
+  const {top} = useSafeAreaInsets();
 
-  const { nowPlaying, isLoading } = useMovies();
+  const {nowPlaying, isLoading} = useMovies();
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
+      <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
         <ActivityIndicator color="red" size={100} />
       </View>
     );
   }
 
   return (
-    <ScrollView>
-      <View style={{ marginTop: top + 20 }}>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={{marginTop: top + 20}}>
         <Carousel
-          layout={"default"}
+          layout={'default'}
           data={nowPlaying}
-          renderItem={({ item }) =>
+          renderItem={({item}) => (
             <MoviePoster movie={item} width={windowsWidth * 0.7} />
-          }
+          )}
           sliderWidth={windowsWidth}
           itemWidth={300}
+          inactiveSlideOpacity={0.9}
         />
 
-        <View style={{ backgroundColor: "red", height: 260 }}>
-          <Text style={{ fontSize: 30, fontWeight: "bold" }}>In cine</Text>
-          <FlatList
-            data={nowPlaying}
-            renderItem={({ item }) =>
-              <MoviePoster movie={item} height={200} width={140} />
-            }
-            keyExtractor={(item) => item.id.toString()}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          />
-        </View>
+        <HorizontalSlider movies={nowPlaying} title="In cine" />
+
+        <HorizontalSlider movies={nowPlaying} title="Hot" />
+
+        <HorizontalSlider movies={nowPlaying} title="Rotten tomatos" />
       </View>
     </ScrollView>
   );
