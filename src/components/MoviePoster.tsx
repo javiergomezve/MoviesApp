8a7d210adcbff8ce76efa2da96d0f33e948f9230
Javier Gomez <javiergomezve@gmail.com>
2024-02-20 10:23:26 -0500

    Add movies carousel

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
 
-import {Movie} from '../interfaces/movieInterface.tsx';
+import { Movie } from "../interfaces/movieInterface.tsx";
 
 interface Props {
   movie: Movie;
+  height?: number;
+  width?: number;
 }
 
-const MoviePoster = ({movie}: Props) => {
+const MoviePoster = (props: Props) => {
+  const { movie, height = 420, width = 300 } = props;
+
   const posterUri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <View style={styles.imageContainer}>
+    <View style={{height, width}}>
       <View style={styles.imageShadow}>
-        <Image source={{uri: posterUri}} style={styles.image} />
+        <Image source={{ uri: posterUri }} style={styles.image} />
       </View>
     </View>
   );
@@ -22,24 +26,22 @@ const MoviePoster = ({movie}: Props) => {
 export default MoviePoster;
 
 const styles = StyleSheet.create({
-  imageContainer: {
-    width: 200,
-    height: 400,
-  },
   imageShadow: {
     flex: 1,
     borderRadius: 18,
-    shadowColor: '#000',
+    marginVertical: 10,
+    marginHorizontal: 5,
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2
     },
     shadowOpacity: 0.9,
     shadowRadius: 3.84,
-    elevation: 10,
+    elevation: 10
   },
   image: {
     flex: 1,
-    borderRadius: 18,
-  },
+    borderRadius: 18
+  }
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,32 +1,60 @@
-import React from 'react';
-import {ActivityIndicator, View} from 'react-native';
-import {StackScreenProps} from '@react-navigation/stack';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import React from "react";
+import { ActivityIndicator, Dimensions, FlatList, ScrollView, Text, View } from "react-native";
+import { StackScreenProps } from "@react-navigation/stack";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Carousel from "react-native-snap-carousel";
 
-import useMovies from '../hooks/useMovies.tsx';
-import MoviePoster from '../components/MoviePoster.tsx';
 
-interface Props extends StackScreenProps<any, any> {}
+import useMovies from "../hooks/useMovies.tsx";
+import MoviePoster from "../components/MoviePoster.tsx";
+
+const { width: windowsWidth } = Dimensions.get("window");
+
+interface Props extends StackScreenProps<any, any> {
+}
 
 const HomeScreen = (props: Props) => {
-  const {navigation} = props;
+  const { navigation } = props;
 
-  const {top} = useSafeAreaInsets();
+  const { top } = useSafeAreaInsets();
 
-  const {nowPlaying, isLoading} = useMovies();
+  const { nowPlaying, isLoading } = useMovies();
 
   if (isLoading) {
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+      <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
         <ActivityIndicator color="red" size={100} />
       </View>
     );
   }
 
   return (
-    <View style={{marginTop: top + 20}}>
-      <MoviePoster movie={nowPlaying[1]} />
-    </View>
+    <ScrollView>
+      <View style={{ marginTop: top + 20 }}>
+        <Carousel
+          layout={"default"}
+          data={nowPlaying}
+          renderItem={({ item }) =>
+            <MoviePoster movie={item} width={windowsWidth * 0.7} />
+          }
+          sliderWidth={windowsWidth}
+          itemWidth={300}
+        />
+
+        <View style={{ backgroundColor: "red", height: 260 }}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>In cine</Text>
+          <FlatList
+            data={nowPlaying}
+            renderItem={({ item }) =>
+              <MoviePoster movie={item} height={200} width={140} />
+            }
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
